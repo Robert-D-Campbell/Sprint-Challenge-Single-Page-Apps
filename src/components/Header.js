8236eb21b1c242dfcfc355e1eec15022e02d1ddc
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-10-18 11:35:21 -0700

    added subheading to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,8 @@ const StyledHeader = styled.header`
   position: absolute;
   top: 35%;
   left: 25%;
-  width: 20%;
+  max-width: 60%;
+  width: 485px;
   h1 {
     font-size: 5rem;
     color: #b5edfc;
